6136abafcb513b3b31f05e2e5e25ed9733d17bb8
Sébastien Champoux <contact@sebastienchampoux.com>
2021-10-06 15:13:17 -0400

    Add month and year events on timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,21 +6,38 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     [SerializeField] private float _dayDurationInSeconds = 1.0f;
+    [SerializeField] private int _daysPerMonth = 30;
+    [SerializeField] private int _monthsPerYear = 6;
+    
     private bool _isRunning = true;
+    private int _currentDay = 1;
+    private int _currentMonth = 1;
 
     public event EventHandler OnNewDay;
+    public event EventHandler OnNewMonth;
+    public event EventHandler OnNewYear;
     public float DayDurationSeconds
     {
         get
         {
             return _dayDurationInSeconds;
         }
-        private set
+        set
         {
             _dayDurationInSeconds = Mathf.Max(0f, value);
         }
     }
     
+    public int DaysPerMonth
+    {
+        get { return _daysPerMonth; }
+    }
+    
+    public int MonthsPerYear
+    {
+        get { return _monthsPerYear; }
+    }
+
     private void Start()
     {
         StartCoroutine(StartTimer());
@@ -31,7 +48,23 @@ public class Timer : MonoBehaviour
         while (_isRunning)
         {
             yield return new WaitForSeconds(DayDurationSeconds);
+
+            _currentDay++;
             OnNewDay?.Invoke(this, null);
+
+            bool isNewMonth = _currentDay >= DaysPerMonth;
+            bool isNewYear = _currentMonth >= MonthsPerYear;
+            if (isNewMonth)
+            {
+                _currentMonth++;
+                _currentDay = 0;
+                OnNewMonth?.Invoke(this, null);
+            }
+            if (isNewYear)
+            {
+                _currentMonth = 0;
+                OnNewYear?.Invoke(this, null);
+            }
         }
     }
 }
--- Assets/Tests/TimerTest.cs
+++ Assets/Tests/TimerTest.cs
@@ -7,37 +7,75 @@ using UnityEngine.TestTools;
 public class TimerTest
 {
 
-    private Timer timer;
-    private double numberOfCallsToNotify = 0;
+    private Timer _timer;
+    private double _numberOfCallsToNotifyDay = 0;
+    private bool _newMonthWasCalled = false;
+    private bool _newYearWasCalled = false;
 
     [SetUp]
     public void beforeTests()
     {
         GameObject timerGO = new GameObject();
         timerGO.AddComponent<Timer>();
-        timer = timerGO.GetComponent<Timer>();
-    }
-
-    [Test]
-    public void shouldHaveNewDayEvent()
-    {
-        timer.OnNewDay += NotifyOnNewDay;
+        _timer = timerGO.GetComponent<Timer>();
+        _timer.DayDurationSeconds = 0.01f;
     }
 
     [UnityTest]
-    public IEnumerator shouldCallSubscribersOnUpdate()
+    public IEnumerator shouldNotifyOnNewDay()
     {
-        numberOfCallsToNotify = 0;
-        timer.OnNewDay += NotifyOnNewDay;
-        Assert.AreEqual(0, numberOfCallsToNotify);
-        yield return new WaitForSeconds(timer.DayDurationSeconds);
-        Assert.AreEqual(1, numberOfCallsToNotify);
-        yield return new WaitForSeconds(timer.DayDurationSeconds);
-        Assert.AreEqual(2, numberOfCallsToNotify);
+        _numberOfCallsToNotifyDay = 0;
+        _timer.OnNewDay += NotifyOnNewDay;
+        Assert.AreEqual(0, _numberOfCallsToNotifyDay);
+        yield return new WaitForSeconds(_timer.DayDurationSeconds);
+        Assert.AreEqual(1, _numberOfCallsToNotifyDay);
+        yield return new WaitForSeconds(_timer.DayDurationSeconds);
+        Assert.AreEqual(2, _numberOfCallsToNotifyDay);
+    }
+
+    [UnityTest]
+    public IEnumerator shouldNotifyOnNewMonth()
+    {
+        Assert.IsFalse(_newMonthWasCalled);
+        _timer.OnNewMonth += Timer_OnNewMonth;
+
+        // Wait for a month to elapse
+        for(int i = 0; i < _timer.DaysPerMonth; i++)
+        {
+            yield return new WaitForSeconds(_timer.DayDurationSeconds);
+        }
+        Assert.IsTrue(_newMonthWasCalled);
+    }
+
+    [UnityTest]
+    public IEnumerator shouldNotifyOnNewYear()
+    {
+        Assert.IsFalse(_newYearWasCalled);
+        _timer.OnNewYear += Timer_OnNewYear;
+
+        // Wait for a year to elapse
+        for(int j = 0; j < _timer.MonthsPerYear; j++)
+        {
+            for (int i = 0; i < _timer.DaysPerMonth; i++)
+            {
+                yield return new WaitForSeconds(_timer.DayDurationSeconds);
+            }
+        }
+        Assert.IsTrue(_newYearWasCalled);
+    }
+
+    private void Timer_OnNewMonth(object sender, System.EventArgs e)
+    {
+        _newMonthWasCalled = true;
+    }
+
+    private void Timer_OnNewYear(object sender, System.EventArgs e)
+    {
+        _newYearWasCalled = true;
     }
 
     public void NotifyOnNewDay(object sender, System.EventArgs e)
     {
-        numberOfCallsToNotify++;
+        _numberOfCallsToNotifyDay++;
     }
 }
