14d20756faf2a13edd875688108e3e01d02c2379
Sébastien Champoux <contact@sebastienchampoux.com>
2021-11-09 15:38:07 -0500

    Refactor Employee tests

--- Assets/Tests/EmployeeTest.cs
+++ Assets/Tests/EmployeeTest.cs
@@ -1,18 +1,68 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
-public class EmployeeTest : MonoBehaviour
+public class EmployeeTest
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ConcreteEmployee : Employee
     {
-        
+        public override void OnNewYear(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private MockTimer _timer;
+    private MockPark _park;
+    private Employee _employee;
+
+    [SetUp]
+    public void SetUpTests()
     {
-        
+        GameObject tempGameObject = new GameObject();
+
+        tempGameObject.AddComponent<MockTimer>();
+        _timer = tempGameObject.GetComponent<MockTimer>();
+
+        tempGameObject.AddComponent<ConcreteEmployee>();
+        _employee = tempGameObject.GetComponent<ConcreteEmployee>();
+
+        tempGameObject.AddComponent<MockPark>();
+        _park = tempGameObject.GetComponent<MockPark>();
+
+        _employee.Park = _park;
+    }
+
+    [Test]
+    public void shouldHaveMontlyWageProp()
+    {
+        Assert.GreaterOrEqual(_employee.MonthlySalary, 0f);
+    }
+
+    [Test]
+    public void shouldBePaidFromParkBankrollMonthly()
+    {
+        _park.AddToBankroll(500f);
+        const float monthlySalary = 100f;
+        _employee.MonthlySalary = monthlySalary;
+        _timer.OnNewMonth += _employee.OnNewMonth;
+
+        Assert.IsFalse(_park.SpendMoneyLastCalledWith(monthlySalary));
+        _timer.ElapseMonth();
+        Assert.IsTrue(_park.SpendMoneyLastCalledWith(monthlySalary));
+    }
+
+    [Test]
+    public void shouldBeFurloughedIfParkCantPaySalary()
+    {
+        const float monthlySalary = 100f;
+        _employee.MonthlySalary = monthlySalary;
+        _park.AddToBankroll(50f);
+        _timer.OnNewMonth += _employee.OnNewMonth;
+
+        Assert.IsFalse(_park.LastFurloughedEmployeeWas(_employee));
+        _timer.ElapseMonth();
+        Assert.IsTrue(_park.LastFurloughedEmployeeWas(_employee));
     }
 }
--- Assets/Tests/SocialMediaManagerTest.cs
+++ Assets/Tests/SocialMediaManagerTest.cs
@@ -28,30 +28,6 @@ public class SocialMediaManagerTest
     }
 
     [Test]
-    public void shouldHaveMontlyWageProp()
-    {
-        Assert.GreaterOrEqual(_smManager.MonthlySalary, 0f);
-    }
-
-    [Test]
-    public void shouldStartNewAdCampaignEachYear()
-    {
-        GameObject temp = new GameObject();
-        temp.AddComponent<AdvertisingCampaign>();
-        AdvertisingCampaign campaign = temp.GetComponent<AdvertisingCampaign>();
-        campaign.MonthlyCost = 100f;
-        _smManager.PossibleCampaignsPrefabs = new AdvertisingCampaign[] { campaign };
-
-        // Park bankroll must be sufficient to start campaign
-        _park.AddToBankroll(1000f);
-        
-        _timer.OnNewYear += _smManager.OnNewYear;
-        Assert.IsFalse(_park.StartAdCampaignWasCalled());
-        _timer.ElapseYear();
-        Assert.IsTrue(_park.StartAdCampaignWasCalled());
-    }
-
-    [Test]
     public void shouldBePaidFromParkBankrollMonthly()
     {
         _park.AddToBankroll(500f);
@@ -62,18 +38,5 @@ public class SocialMediaManagerTest
         Assert.IsFalse(_park.SpendMoneyLastCalledWith(monthlySalary));
         _timer.ElapseMonth();
         Assert.IsTrue(_park.SpendMoneyLastCalledWith(monthlySalary));
-    }
-
-    [Test]
-    public void shouldBeFurloughedIfParkCantPaySalary()
-    {
-        const float monthlySalary = 100f;
-        _smManager.MonthlySalary = monthlySalary;
-        _park.AddToBankroll(50f);
-        _timer.OnNewMonth += _smManager.OnNewMonth;
-
-        Assert.IsFalse(_park.LastFurloughedEmployeeWas(_smManager));
-        _timer.ElapseMonth();
-        Assert.IsTrue(_park.LastFurloughedEmployeeWas(_smManager));
     }
 }
